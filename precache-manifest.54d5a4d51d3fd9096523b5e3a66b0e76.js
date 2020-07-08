34710711e83324df0e6f088d2f07372f3dad3c29
Craig Barry <craigfbarry@gmail.com>
2020-07-08 22:10:54 +0930

    Updates

---
+++ precache-manifest.54d5a4d51d3fd9096523b5e3a66b0e76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23084b3ddbadde293c054631891e31c5",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "61191541e46118ed7672",
+    "url": "/user-directory/static/css/main.4fdda61c.chunk.css"
+  },
+  {
+    "revision": "947e6a6ca174432019a7",
+    "url": "/user-directory/static/js/2.b6685048.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-directory/static/js/2.b6685048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61191541e46118ed7672",
+    "url": "/user-directory/static/js/main.60ed7a89.chunk.js"
+  },
+  {
+    "revision": "fd49b7346e1110e7173a",
+    "url": "/user-directory/static/js/runtime-main.d986fb6f.js"
+  }
+]);
